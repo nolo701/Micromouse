37118a962aabd004763113082058653d2db32d88
ryeso <93949117+ryeso@users.noreply.github.com>
2022-09-12 19:14:24 -0400

    Update Motor.h

--- StateMachine/FunctionLibrary/Motor.h
+++ StateMachine/FunctionLibrary/Motor.h
@@ -1,8 +1,7 @@
 #include "Motor.c"
 
 void moveMotor(int [], char, int);
-void enableMotors(void);
 void stopMotors(int [], int []);
 void moveForward(int,int [],int []);
-void turnLeft(int duty,int diff, int motorL[],int motorR[]);
-void turnRight(int duty,int diff, int motorL[],int motorR[]);
+void turnLeft(int,int, int motorL[],int motorR[]);
+void turnRight(int,int, int [],int []);
