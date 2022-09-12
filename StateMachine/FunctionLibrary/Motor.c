b91effd7f94b00b60025578cf48718378e6241a4
ryeso <93949117+ryeso@users.noreply.github.com>
2022-09-12 18:26:27 -0400

    Updated motor library
    Added motor functions: turnLeft, turnRight, moveForward

--- StateMachine/FunctionLibrary/Motor.c
+++ StateMachine/FunctionLibrary/Motor.c
@@ -3,13 +3,11 @@
 
 // int motor[] = {MotorCh1, MotorCh2, MotorEN, Ticks,   Vco }
 //                motor[0]  motor[1]  motor[2] motor[3] motor[4]
+
+
 void moveMotor(int motor[], char dir, int duty){
     digitalWrite(motor[2],HIGH);
-    int numDir = dir;
-    //If the current speed does not match the
-    if(duty != motor[3]){
-
-    }
+    int numDir = dir;   
     int bitDuty = 255*duty/100;
     //This will turn on the corresponding motor depending on direction
     analogWrite(motor[0],(numDir=='f')*bitDuty);
@@ -25,3 +23,22 @@ void stopMotors(int motorL[], int motorR[]){
     digitalWrite(motorL[2],LOW);
     digitalWrite(motorR[2],LOW);
 }
+
+void moveForward(int duty, int motorL[],int motorR[]){  
+	moveMotor(motorL,'f',duty);
+    moveMotor(motorR,'f',duty);
+	return;
+}
+
+void turnLeft(int duty,int diff, int motorL[],int motorR[]){  //diff should be <1 and determines the ratio of motor speeds
+			moveMotor(motorL,'f',diff*duty);
+			moveMotor(motorR,'f',duty);
+			return;
+}
+
+void turnRight(int duty,int diff, int motorL[],int motorR[]){  //diff should be <1 and determines the ratio of motor speeds
+			moveMotor(motorL,'f',duty);
+			moveMotor(motorR,'f',diff*duty);
+			return;
+}
+
--- StateMachine/FunctionLibrary/Motor.h
+++ StateMachine/FunctionLibrary/Motor.h
@@ -2,3 +2,7 @@
 
 void moveMotor(int [], char, int);
 void enableMotors(void);
+void stopMotors(int [], int []);
+void moveForward(int,int [],int []);
+void turnLeft(int duty,int diff, int motorL[],int motorR[]);
+void turnRight(int duty,int diff, int motorL[],int motorR[]);
