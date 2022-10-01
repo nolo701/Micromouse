d72d64258e16ac238f2e8b2bd030541bc7010c79
benbobben <94030209+benbobben@users.noreply.github.com>
2022-10-01 16:16:32 -0400

    Add files via upload
    NewPing is now first in Function Lib

---
+++ FunctionLibrary.h
@@ -0,0 +1,33 @@
+//#ifndef <Arduino.h>
+//    #define <Arduino.h>
+//#endif
+//#ifndef "Motor.h"
+
+#ifndef NEWPING_CPP
+#include "NewPing.cpp"
+#endif
+
+#ifndef MOTOR_H
+#include "Motor.h"
+#endif
+
+
+#ifndef SENSORS_H
+#include "Sensors.h"
+#endif
+
+#ifndef THINKING_H
+#include "Thinking.h"
+#endif
+
+#ifndef MAZE_H
+#include "Maze.h"
+#endif
+
+#ifndef STDLIB_H
+#include <stdlib.h>
+#endif
+
+#ifndef STDBOOL_H
+#include <stdbool.h>
+#endif
